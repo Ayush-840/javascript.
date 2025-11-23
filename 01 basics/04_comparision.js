67f291fd14f4dfef2357800e1da2768d89bb02ad
Ayush Singh <studentuse33@gmail.com>
2025-11-23 08:08:17 +0000

    added 4th comparision

--- 01 basics/04_comparision.js
+++ 01 basics/04_comparision.js
@@ -1,62 +1,3 @@
-let score = 33
-console.log(typeof score)    //output; number
-console.log(typeof (score))   //output; number
-
-
-let scores = "33jhBDHJ"
-let valueInNumber=Number(scores)
-console.log(typeof valueInNumber);    //output; number
-console.log(valueInNumber);   //output; NaN
-
-
-let scor = null
-let valueInNumbers=Number(scor)
-console.log(typeof valueInNumbers);    //output; number
-console.log(valueInNumbers);         // output; 0
-
-
-let scorr = undefined
-let valueInNumberr=Number(scorr)
-console.log(typeof valueInNumberr);    //output; number
-console.log(valueInNumberr);       // output; NaN
-
-
-let scorrr = true
-let valueInNumberrr=Number(scorrr)
-console.log(typeof valueInNumberrr);    //output; number
-console.log(valueInNumberrr);        // output ; 1
-
-// 33 => 33
-// "33abcdd" => NaN
-// true => 1
-// false => 0
-
-
-//__________________________________________________//
-
-
-let isLoggedIn=""
-let booleanIsLoggedIn = Boolean(isLoggedIn)   
-console.log(typeof booleanIsLoggedIn);   //output; bollean 
-console.log(booleanIsLoggedIn);     // output; false
-
-let isLoggedInn="Ayush"
-let booleanIsLoggedInn = Boolean(isLoggedInn)  
-console.log(booleanIsLoggedInn);     // output; true
-
-// 1 => true ; 0 => false
-// "" => false
-// "AYush" => true
-
-//_____________________________________________//
-
-
-let number= 99
-let StringNumber = String(number)
-console.log(StringNumber)          //output; 99
-console.log(typeof StringNumber);      //output; string
-
-
 
 //***************** Operations **********************************
 
@@ -121,8 +62,3 @@ console.log(undefined >= 0);     //output; false
        //**** */  === Operators //
 console.log("2"===2);  // false
 // === this operator check everything like data-type, numerical value,and everything
-// === this is very very important
-
-
-
-
