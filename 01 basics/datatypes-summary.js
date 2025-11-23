11f4f8d9e4d6370f1871ed57e6de2b7a80ae68e8
Ayush Singh <studentuse33@gmail.com>
2025-11-23 08:49:48 +0000

    datatype-summary

---
+++ 01 basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+// primitive
+// there are 7 primitive data-types
+// 7 tpes : String, Number, Boolean , null, undefined, Symbol, Bigint
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);      // false
+console.log(typeof id)          // Symbol
+console.log(typeof anotherId)    // Symbol
+
+
+// Reference (non premitive) 
+//  Array , objects , functions,
+// most vvi array and object for mastery in js
+
+
+
