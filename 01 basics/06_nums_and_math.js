01ed608c758b17d1620111aadb3b50f6bb46fb38
Ayush Singh <studentuse33@gmail.com>
2025-11-26 18:23:34 +0000

    nums

---
+++ 01 basics/06_nums_and_math.js
@@ -0,0 +1,10 @@
+const score = 400
+console.log(score)   //output; 400
+
+const balance = new Number(100)
+console.log(balance);     // output; [Number: 100]
+
+console.log(balance.toString())    // output; 100
+
+console.log(balance.toString().length);    // output; 3
+
